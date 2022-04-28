cde071f705624a95a044495c8ea3bb9dc32a9977
Sherreka Burton <rekadarnb@gmail.com>
2022-04-28 14:23:33 -0400

    base file is done

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,4 @@
 
-// [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
@@ -41,9 +40,6 @@ function renderLicenseBadge(data) {
 };
 
 
-
-
-
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(data) {
@@ -77,7 +73,7 @@ function renderLicenseLink(data) {
     licenseLink += `(https://opensource.org/licenses/EPL-2.0)`;
     break; 
     default:
-    licenseLink = '';
+    licenseLink = 'No license in place';
     break;
   }
   return licenseLink;
@@ -95,7 +91,6 @@ function renderLicenseSection(data) {
    `
   return licenseRender;
 }
-
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
